e9f709441357e3849f1b121ba2a62b67919f0a5a
geekgit <geeekz@gmail.com>
2015-12-21 09:13:03 +0500

    SMART function

--- hdd-fun.ps1
+++ hdd-fun.ps1
@@ -1,3 +1,25 @@
+function Show-SMART-Value ($SMARTData,$tag)
+{
+	$arr=$SMARTData.Split("`r`n")
+	foreach($line in $arr)
+	{
+		if($line.Contains($tag))
+		{
+		#Write-Host $line
+		$separator="`t","`r","`n"," "
+		$option = [System.StringSplitOptions]::RemoveEmptyEntries
+		$sub_arr=$line.Split($separator,$option)
+		<# foreach($sub_line in $sub_arr)
+		{
+			Write-Host '< ' $sub_line ' >'
+		} #>
+		$ReturnValueData=$sub_arr[9]
+		return $ReturnValueData;
+		}
+	}
+	return 'N\A'
+}
+$SmartMonToolsPath='C:\Program Files\smartmontools\bin\smartctl.exe'
 $IsAdmin=([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator");
 Write-Host 'Admin: ' $IsAdmin
 if (!$IsAdmin)
@@ -14,9 +36,21 @@ Try
 	Get-PhysicalDisk
 	Write-Host 'MSStorageDriver_FailurePredictStatus'
 	Get-WmiObject -Namespace Root\WMI -Class MSStorageDriver_FailurePredictStatus
+	Write-Host 'smartctl /dev/sda'
+	$output=(& $SmartMonToolsPath -A /dev/sda)
+	$AirflowCelsius=Show-SMART-Value $output 'Airflow_Temperature'
+	Write-Host 'Airflow Temperature: +' $AirflowCelsius 'C'
+	$TemperatureCelsius=Show-SMART-Value $output 'Temperature_Celsius'
+	Write-Host 'Temperature: +' $TemperatureCelsius 'C'
+	$ReallocatedSectors=Show-SMART-Value $output 'Reallocated_Sector_Ct'
+	Write-Host 'Reallocated Sectors:' $ReallocatedSectors
+	$PendingSectors=Show-SMART-Value $output 'Current_Pending_Sector'
+	Write-Host 'Pending Sectors:' $PendingSectors
+	
 }
 Catch
 {
 	Write-Host "Error!"
+	Write-Host $Error
 }
 Read-Host -Prompt "Press Enter to exit..."
