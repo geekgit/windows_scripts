3359255399bb07fc2bf05a58d2b1f1a9b911e576
geekgit <geeekz@gmail.com>
2016-01-17 20:33:31 +0500

    csc script

---
+++ find-csc.ps1
@@ -0,0 +1,16 @@
+function get-csc
+{
+	$PathAL=New-Object System.Collections.ArrayList
+	#VS2015
+	$PathAL.Add("C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\csc.exe") | Out-Null
+	$PathAL.Add("C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe") | Out-Null
+	#
+	foreach($path in $PathAL)
+	{
+		$flag=(Test-Path $path)
+		return $path
+	}
+	return "N/A"
+}
+$csc=(get-csc)
+Write-Host "csc: $csc"
