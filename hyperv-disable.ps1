c086af30f7ab08d817be8b045d8d9ce00efaf8fc
geekgit <geeekz@gmail.com>
2016-08-03 03:44:36 +0000

    hyper-v scripts

---
+++ hyperv-disable.ps1
@@ -0,0 +1,27 @@
+﻿$IsAdmin=([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator");
+Write-Host 'Admin: ' $IsAdmin
+if (!$IsAdmin)
+{
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+	exit; 
+}
+Write-Host 'Disable Hyper-V...'
+Try
+{
+	Write-Host "dism.exe /Online /Disable-Feature:Microsoft-Hyper-V"
+	dism.exe /Online /Disable-Feature:Microsoft-Hyper-V | out-null
+}
+Catch
+{
+	Write-Host $_
+}
+Try
+{
+	Write-Host "bcdedit /set hypervisorlaunchtype off"
+	bcdedit /set hypervisorlaunchtype off | out-null
+}
+Catch
+{
+	Write-Host $_
+}
+Read-Host -Prompt "Press Enter to exit..."
---
+++ hyperv-enable.ps1
@@ -0,0 +1,27 @@
+﻿$IsAdmin=([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator");
+Write-Host 'Admin: ' $IsAdmin
+if (!$IsAdmin)
+{
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+	exit; 
+}
+Write-Host 'Enable Hyper-V...'
+Try
+{
+	Write-Host "dism.exe /Online /Enable-Feature:Microsoft-Hyper-V /All"
+	dism.exe /Online /Enable-Feature:Microsoft-Hyper-V /All | out-null
+}
+Catch
+{
+	Write-Host $_
+}
+Try
+{
+	Write-Host "bcdedit /set hypervisorlaunchtype auto"
+	bcdedit /set hypervisorlaunchtype auto | out-null
+}
+Catch
+{
+	Write-Host $_
+}
+Read-Host -Prompt "Press Enter to exit..."
