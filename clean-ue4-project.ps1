4b0c550692031ccc9f2390477ec4c5b2757f9017
geekgit <geeekz@gmail.com>
2017-06-21 00:29:43 +0000

    clean-ue4-project

---
+++ clean-ue4-project.ps1
@@ -0,0 +1,37 @@
+# Author: geekgit
+# Date: 21.06.2017 0:17 UTC
+Try
+{
+$whiteList=New-Object System.Collections.ArrayList($null);
+$whiteList.Add("content");
+$dirVar=(Get-ChildItem | ?{ $_.PSIsContainer}).Name;
+Write-Host "Dir in project folder: ";
+
+#just print info
+ForEach ($currDir in $dirVar)
+{
+	$currDirLowercase=$currDir.ToLower();
+	$flag=$whiteList.contains($currDirLowercase);
+	Write-Host "Directory $currDir [$currDirLowercase], whitelist? $flag"
+}
+
+#clean
+ForEach ($currDir in $dirVar)
+{
+	$currDirLowercase=$currDir.ToLower();
+	$flag=$whiteList.contains($currDirLowercase);
+	if(!$flag)
+	{
+	Write-Host "Delete $currDir..."
+	Remove-Item .\$currDir -Force -Recurse
+	}
+}
+
+
+}
+Catch
+{
+	$Message=$_
+	Write-Host "Exception: $Message"
+}
+Read-Host -Prompt "Press Enter to exit..."
