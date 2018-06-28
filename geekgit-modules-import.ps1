a95ac63f67beb16d15c434b153d86c873defe9fc
geekgit <geeekz@gmail.com>
2018-06-28 21:33:07 +0000

    update

--- geekgit-modules-import.ps1
+++ geekgit-modules-import.ps1
@@ -1,3 +1,4 @@
 Import-module Geekgit-Pkill
 Import-module Geekgit-7z-Each-Directory-To-Separate-Archive
 Import-module Geekgit-7z-Each-Directory-To-Separate-Archive-Encrypt
+Import-Module Geekgit-Edit-Hosts
