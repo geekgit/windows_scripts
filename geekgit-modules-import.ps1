a09964988d2a68d6c191ac3a8c346b520eacf978
geekgit <geeekz@gmail.com>
2021-01-13 10:27:18 +0000

    import update

--- geekgit-modules-import.ps1
+++ geekgit-modules-import.ps1
@@ -2,3 +2,4 @@ Import-module Geekgit-Pkill
 Import-module Geekgit-7z-Each-Directory-To-Separate-Archive
 Import-module Geekgit-7z-Each-Directory-To-Separate-Archive-Encrypt
 Import-Module Geekgit-Edit-Hosts
+Import-Module Geekgit-FFmpeg-DTS-to-AC3
