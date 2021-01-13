128a5c9492a4b50ef627ddd7e6b0d4572ed4da06
geekgit <geeekz@gmail.com>
2021-01-13 10:19:31 +0000

    Update geekgit-modules-download.ps1
    force directory create

--- geekgit-modules-download.ps1
+++ geekgit-modules-download.ps1
@@ -1,2 +1,4 @@
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
 Set-Location -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
 git clone "https://github.com/geekgit/windows_scripts_modules" .
+ls
