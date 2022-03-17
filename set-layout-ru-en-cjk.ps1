cd0057432c36e4370cbbd8a51d8923e0a2fbd799
geekgit <geeekz@gmail.com>
2022-03-17 22:26:53 +0500

    set-layout-ru-en-cjk

---
+++ set-layout-ru-en-cjk.ps1
@@ -0,0 +1,17 @@
+Try
+{
+$Layout=New-WinUserLanguageList ru
+$Layout.Add("en-US")
+$Layout.Add("ja")
+$Layout.Add("zh-Hans-CN")
+$Layout.Add("ko")
+Set-WinUserLanguageList $Layout -Force
+}
+Catch
+{
+	$Message=$_.Exception.Message
+	Write-Host "Error: $Message"
+	$Fail=$_.Exception.ItemName
+	Write-Host "Failed: $Fail"
+}
+Read-Host -Prompt "Press Enter to exit..."
