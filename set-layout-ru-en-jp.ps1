275daed03c4aa073432b91833bedb98335db8375
geekgit <geeekz@gmail.com>
2022-12-09 15:00:05 +0000

    Create set-layout-ru-en-jp.ps1

---
+++ set-layout-ru-en-jp.ps1
@@ -0,0 +1,15 @@
+Try
+{
+$Layout=New-WinUserLanguageList ru
+$Layout.Add("en-US")
+$Layout.Add("ja")
+Set-WinUserLanguageList $Layout -Force
+}
+Catch
+{
+	$Message=$_.Exception.Message
+	Write-Host "Error: $Message"
+	$Fail=$_.Exception.ItemName
+	Write-Host "Failed: $Fail"
+}
+Read-Host -Prompt "Press Enter to exit..."
