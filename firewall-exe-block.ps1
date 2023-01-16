9dfcf711a530f3d29aad7f37eb6d76e2fa8ee40e
geekgit <geeekz@gmail.com>
2023-01-16 11:07:12 +0000

    Create firewall-exe-block.ps1

---
+++ firewall-exe-block.ps1
@@ -0,0 +1,31 @@
+$IsAdmin=([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator");
+Write-Host 'Admin: ' $IsAdmin
+if (!$IsAdmin)
+{
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+	exit; 
+}
+Write-Host 'Block online access...'
+Try
+{
+    $directory="C:\App"
+    $exename="app.exe"
+    $exepath=$directory+'\'+$exename
+    $appname="App"
+	
+    Write-Host "App: $appname"
+    Write-Host "Exe Path: $exepath"
+    
+    $out="$appname Block $exename (out)"
+    Write-Host "$out..."
+	New-NetFirewallRule -DisplayName $out -Direction Outbound -Action Block -Profile Any -Enabled True -Program $exepath
+    $in="$appname Block $exename (in)"
+    Write-Host "$in..."
+	New-NetFirewallRule -DisplayName $in -Direction Inbound -Action Block -Profile Any -Enabled True -Program $exepath
+}
+Catch
+{
+	$Message=$_
+	Write-Host "Exception: $Message"
+}
+Read-Host -Prompt "Press Enter to exit..."
